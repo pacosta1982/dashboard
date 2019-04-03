1de2728e7b4f33da16708d060222edcb24264d17
Pedro Acosta <acosta.aleoli@gmail.com>
2019-04-03 10:58:56 -0400

    ajustes

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -125,9 +125,9 @@ class MapsController extends Controller
                 '<div id="bodyContent">'.
                 '<p><b>SAT:</b> '.utf8_encode($project->SEOBEmpr).'<br>'.
                 '<b>Departamento:</b> '.$depto.'<br>'.
-                '<b>Distrito:</b> '.$distrito.'<br>'.
+                /*'<b>Distrito:</b> '.$distrito.'<br>'.
                 '<b>Estado:</b> '.$project->SEOBEst.'<br>'.
-                /*'<b>Total Casas:</b> '.$project->SEOBViv.'<br>'.
+                '<b>Total Casas:</b> '.$project->SEOBViv.'<br>'.
                 '<b>Total Avance:</b> '.$avance.'%<br></p>'.
                 '<div class="progress-bar bg-success" style="width: '.$project->SEOBFisAva.'% "></div>'.
                 '<p>Resumen del Proyecto:'.'<a href="/home/'.$project->SEOBId.'">'.utf8_encode($project->SEOBProy).'</a>'.
